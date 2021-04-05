da1cb6428d4748991542ac78a51a156f9ec01b55
BrianARuff <briananthonyruff@gmail.com>
2021-04-05 11:11:37 -0400

    use base react

--- ImageUploader.js
+++ ImageUploader.js
@@ -1,21 +1,17 @@
 import { useState } from "react";
-
 const ImageUploader = () => {
   const [image, setImage] = useState(null);
   const handleImageUpload = (e) => {
     const url = URL.createObjectURL(e.target.files[0]);
     setImage(url);
   };
-  return (
-    <article>
-      <input onChange={handleImageUpload} type="file" />
-      {image ? (
-        <img src={image} alt="image preview" />
-      ) : (
-        <p>Please Upload Image</p>
-      )}
-    </article>
+  return React.createElement(
+    "article",
+    null,
+    React.createElement("input", { onChange: handleImageUpload, type: "file" }),
+    image
+      ? React.createElement("img", { src: image, alt: "image preview" })
+      : React.createElement("p", null, "Please Upload Image")
   );
 };
-
 export default ImageUploader;
